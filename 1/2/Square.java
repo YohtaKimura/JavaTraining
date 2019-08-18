0479947384ac067a1057c39fe90676a337ea43e8
YohtaKimura <yohta.kimura@nts.ricoh.co.jp>
2019-08-18 23:39:50 +0900

    Add Fibonacci2

---
+++ 1/2/Square.java
@@ -0,0 +1,11 @@
+class Square {
+    /** 値が 50 未満の平方数を表示する */
+    public static void main(String[] args) {
+        System.out.println("Title: Following are squared numbers");
+        int square = 0;
+        for (int i = 0; i*i < 50; i++) {
+            square = i * i;
+            System.out.println(String.valueOf(i) + ": " + String.valueOf(square));
+        }
+    }
+}
---
+++ 1/4/HelloWorld2.java
@@ -0,0 +1,7 @@
+class HelloWorld2 {
+    static final String GREETING = "Hello, world";
+
+    public static void main(String[] args) {
+        System.out.println(GREETING);
+    }
+}
