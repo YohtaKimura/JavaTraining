fd1d4235052db380db8b5dd200d47c8c9640778d
YohtaKimura <yohta.kimura@nts.ricoh.co.jp>
2019-08-18 22:13:30 +0900

    Add error

---
+++ 1/1/Error.java
@@ -0,0 +1,5 @@
+class Error {
+    public static void main(String[] args) {
+        null.println("Hello, world");
+    }
+}
---
+++ 1/1/Error2.java
@@ -0,0 +1,6 @@
+class Error2 {
+    public static void main(String[] args) {
+        String a = null;
+        System.out.println("Hello, world" + a.length());
+    }
+}
